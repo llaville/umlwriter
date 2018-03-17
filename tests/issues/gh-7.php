c3863e4f38e3b82a8afb9c447add3cef82b322cd
Laurent Laville <llaville.net@gmail.com>
2018-03-17 09:14:37 +0100

    script test for issue GH #7

---
+++ tests/issues/gh-7.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+namespace ltta\model;
+class BackupProject
+{
+    private $a;
+    public function __construct(string  $a)
+    {
+        $this->a = $a;
+    }
+}
