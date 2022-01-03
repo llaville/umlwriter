922d7d6a7d4e86aa9c6328fb3ecb1a9702821fb1
Laurent Laville <laurent.laville@gmail.com>
2022-01-03 11:22:45 +0000

    update services examples

---
+++ resources/graph-uml/filter/services.php
@@ -0,0 +1,12 @@
+<?php declare(strict_types=1);
+/**
+ * This file is part of the UmlWriter package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @since Release 3.0.0
+ * @author Laurent Laville
+ */
+
+// no filter required
