94f7305b8d37e5b60c029ba23460615ac0616afe
Laurent Laville <laurent.laville@gmail.com>
2023-12-21 14:14:54 +0000

    split bootstrapping logic from CLI application launcher

---
+++ bootstrap.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types=1);
+// @link https://www.tomasvotruba.cz/blog/2018/08/02/5-gotchas-of-the-bin-file-in-php-cli-applications/
+
+gc_disable(); // performance boost
+
+$autoloader = 'vendor/autoload.php';
+
+if (Phar::running()) {
+    $phar = new Phar($_SERVER['argv'][0]);
+    $possibleAutoloadPaths = [
+        'phar://' . $phar->getAlias(),
+    ];
+
+} else {
+    $possibleAutoloadPaths = [
+        // local dev repository
+        __DIR__,
+        // dependency
+        dirname(__DIR__, 3),
+    ];
+}
+
+$isAutoloadFound = false;
+foreach ($possibleAutoloadPaths as $possibleAutoloadPath) {
+    if (file_exists($possibleAutoloadPath . DIRECTORY_SEPARATOR . $autoloader)) {
+        require_once $possibleAutoloadPath . DIRECTORY_SEPARATOR . $autoloader;
+        $isAutoloadFound = true;
+        break;
+    }
+}
+
+if ($isAutoloadFound === false) {
+    throw new RuntimeException(
+        sprintf(
+            'Unable to find "%s" in "%s" paths.',
+            $autoloader,
+            implode('", "', $possibleAutoloadPaths)
+        )
+    );
+}
