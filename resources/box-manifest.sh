0551e4784b8b5576ca140c3b581fdd2f0f637b5a
Laurent Laville <laurent.laville@gmail.com>
2026-04-18 05:36:49 +0000

    upgrade BOX Manifest to latest version (4.4) and add new Composer manifest text copy

--- resources/box-manifest.sh
+++ resources/box-manifest.sh
@@ -2,8 +2,8 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-BOX_MANIFEST_VERSION="4.2.0"
+BOX_MANIFEST_VERSION="4.4.0"
 curl -Ls "https://github.com/llaville/box-manifest/releases/download/$BOX_MANIFEST_VERSION/box-manifest.phar" -o $SCRIPT_DIR/box-manifest
 chmod +x $SCRIPT_DIR/box-manifest
 
-$SCRIPT_DIR/box-manifest make build stub configure -r console-table.txt -r plain.txt -r sbom.json -d $SCRIPT_DIR/.. --output-stub stub.php --output-conf box.json.dist -vvv --ansi
+$SCRIPT_DIR/box-manifest make build stub configure -r console-table.txt -r plain.txt -r sbom.json -r pkg.composer.txt -d $SCRIPT_DIR/.. --output-stub stub.php --output-conf box.json.dist -vvv --ansi
